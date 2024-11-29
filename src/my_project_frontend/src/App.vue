bdea12008bc11cd004846703f84e5366464812cb
Sebastian5141 1 <komorsebastian778@gmail.com>
2024-11-29 12:22:39 +0000

    zadanie3

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
 import { ref } from 'vue';
 import { my_project_backend } from 'declarations/my_project_backend/index';
-let displasyChat = ref([]);
-
+let displayChat = ref([]);
+ 
 async function handleSubmit(e) {
   e.preventDefault();
   const target = e.target;
@@ -10,16 +10,16 @@ async function handleSubmit(e) {
   await my_project_backend.save_msg(msg)
   await getChat()
 }
-
+ 
 async function getChat() {
-  displasyChat.value = await my_project_backend.get_chat()
+  displayChat.value = await my_project_backend.get_chat()
 }
-
+ 
 getChat()
 </script>
-
+ 
 <template>
-  <main>
+  <main class="container mx-auto flex justify-center items-center flex-col p-6 border-2 my-6 w-fix rounded-xl">
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
@@ -28,9 +28,8 @@ getChat()
       <input id="msg" alt="msg" type="text" />
       <button type="submit">Click Me!</button>
     </form>
-    <section id="displasyChat">
-      <div v-for="msg in displasyChat">{{ msg }}</div>
+    <section id="displayChat" class="w-full">
+    <div v-for="msg in displayChat" class="flex justify-start">{{ msg }}</div>
     </section>
   </main>
 </template>
-
